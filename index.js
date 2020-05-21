89f636d315e86146eec6130fb5908183d8423881
btrz <btrz.developer@gmail.com>
2020-05-21 19:16:03 +0200

    Serving statics at public folder with inert package

--- index.js
+++ index.js
@@ -1,33 +1,53 @@
 'use strict'
-
-const Hapi = require('hapi');
 const config = require('./config');
+const Hapi = require('hapi');
+const inert = require('inert'); // serves static files
+const path = require('path');
 
 const server = Hapi.server({
   port: config.serverPort,
-  host: config.host
+  host: config.host,
+  routes: {
+      files : {
+          relativeTo: path.join(__dirname, 'public') // inert?
+      }
+  }
 });
 
 async function init () {
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (req, h) => {
-            return h.response('Hapi to return something!')
-                    .code(200);
-        }
-    });
-
-    server.route({
-        method: 'GET',
-        path: '/i-want-to-see-madonna-live',
-        handler: (req, h) => {
-            return h.redirect('https://www.youtube.com/watch?v=Z26VOATBwhM')
-        }
-    });
-
     try {
+        // Happi needs to initialize plugins before server starts
+        await server.register(inert);
+        // next two routes are related to inert: sets index, and serves all public folder statics so that getting the front.
+        server.route({
+            method: 'GET',
+            path: '/home',
+            handler: (req, h) => {
+                return h.file('index.html');
+            }
+        });
+
+        server.route({
+            method: 'GET',
+            path: '/{param*}',
+            handler: {
+                directory: {
+                    path: '.', // this means 'public' as is setted above.
+                    index: ['index.html']
+                }
+            }
+        });
+    
+        server.route({
+            method: 'GET',
+            path: '/i-want-to-see-madonna-live',
+            handler: (req, h) => {
+                return h.redirect('https://www.youtube.com/watch?v=Z26VOATBwhM')
+            }
+        });
+
         await server.start();
+
     } catch (error) {
         console.error(error);
         process.exit(1); // that 1 is used when exits at an error block
