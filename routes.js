2f8a70e3c02145032c229ee01d463508516e2a0b
btrz <btrz.developer@gmail.com>
2020-05-22 19:40:05 +0200

    Joi payload data validation at registration

--- routes.js
+++ routes.js
@@ -1,7 +1,8 @@
 'use strict'
 const site = require('./controllers/site');
 const user = require('./controllers/user');
-//This is going to be a routes array! Without calling  each time
+const Joi = require('joi');
+
 module.exports = [
     {
         method: 'GET',
@@ -15,6 +16,17 @@ module.exports = [
     },
     {
         method: 'POST',
+        options: {
+            validate : {
+                payload: {
+                    name: Joi.string().required().min(3),
+                    email: Joi.string().email().required(),
+                    password: Joi.string().required().min(6),
+
+
+                }
+            }
+        },
         path: '/create-user',
         handler: user.createUser
     },   
